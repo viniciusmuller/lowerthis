84f97058462dff4a9e63404cd0838565a938b682
arcticlimer <vinigm.nho@gmail.com>
2020-08-28 06:55:28 -0300

    Refactored

--- extension/app/content.js
+++ extension/app/content.js
@@ -1,36 +1,35 @@
-function WaitForVideo(){
-    
-    const interval = setInterval(async function() {
-        let url = window.location.href;
-        if (url.startsWith('https://www.instagram.com/stories')){
-            let video = document.querySelector("video");
-            if (video) {
-                let userVolumeObj = await LoadFromAPIStorage("userVolume");
-                userVolume = userVolumeObj["userVolume"];
-                ChangeVideoVolume(video, userVolume / 100);
-                clearInterval(interval);
-            }
+// ALERT: Despite this extension is not properly packed by Chrome,
+// this content script may not run on the browser, if that happens,
+// the best thing you can do is try to reload the extension files 
+// and restart Chrome.
+
+// Main background interval
+setInterval(async () => {
+    console.log('started');
+    let url = window.location.href;
+    // Check if the page is actually a instagram story;
+    if (url.startsWith('https://www.instagram.com/stories')){
+        let video = document.querySelector("video");
+        // If there's a story running on the page;
+        if (video) {
+            let userVolume = (await LoadFromAPIStorage("userVolume"))["userVolume"];
+            // Load the volume value from API and sets
+            // it into the current video volume;
+            video.volume = userVolume / 100;
         }
-    }, 150);
-}
+    }
+}, 150);
 
-function ChangeVideoVolume(video, volume){
-    video.volume = volume;
-    WaitForVideo();
-}
-
+// Receives a key parameter and get it from the google
+// extension storage API.
 async function LoadFromAPIStorage(key) {
     return new Promise((resolve, reject) => {
         try {
-            chrome.storage.sync.get(key, function (value) {
+            chrome.storage.sync.get(key, value => {
                 resolve(value);
-            })
+            });
         } catch (error) {
             reject(error);
         }
     });
 }
-
-WaitForVideo();
-
-
--- extension/app/popup.js
+++ extension/app/popup.js
@@ -1,21 +1,29 @@
-document.addEventListener("DOMContentLoaded", function(){  // Wait page to fully load before start;
+// Wait page to fully load before start;
+document.addEventListener("DOMContentLoaded", () => {
 
+    // Taking elements and values;
     const userVolumeInput = document.querySelector("#ext-volume-slider");
     const github_url = document.querySelector('a');
     userVolumeInput.value = localStorage.getItem('sliderValue');
 
-    github_url.addEventListener("click", function(){
+    // Adding GitHub profile link on popup;
+    github_url.addEventListener("click", () => {
         let win = window.open("https://github.com/arcticlimer", "_blank");
         win.focus();
     })
 
     let lastUserVolume;
-    const interval = setInterval(function() {
+    // Checking if the current volume is different from the last
+    // user volume before save it into storage to save API calls.
+    setInterval( () => {
         let userVolume = userVolumeInput.value;
+        // If the volume is different from the last check, then save it.
         if (userVolume != lastUserVolume){
             chrome.storage.sync.set({"userVolume": userVolume});
         }
+        // Current volume turns into the last for the next iteration;
         lastUserVolume = userVolume;
+        // Saving the slider value locally to save API calls;
         window.localStorage.setItem('sliderValue', userVolume);
     }, 100);
 });
